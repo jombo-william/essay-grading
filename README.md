addfcad32c6bf53893cf39c89520a7ddad574a26
jombo-william <jombowilliam24@gmail.com>
2026-03-28 23:50:57 -0600

    clear main branch - content will come from merges

--- README.md
+++
@@ -1,6 +0,0 @@
-# Essay Grading Frontend
-## Security
-Never commit .env files.
-## Setup
-1. npm install
-2. npm run dev
--- jombo-essaygrade/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
-//C:\Users\BR\Desktop\final_p\jombo-essaygrade\postcss.config.js
