8b59ea23948689d2e770e1b9b2a63b9f201bdbfd
TNYASULU <bed-com-26-21>
2026-05-29 07:42:53 +0200

    UPDATE

--- main-folder/src/config.js
+++ main-folder/src/config.js
@@ -1,5 +1,5 @@
 const configuredApiUrl = import.meta.env.VITE_API_URL;
 
-const API_URL = (configuredApiUrl || "https://jombo-essaygrade.fly.dev").replace(/\/$/, "");
+const API_URL = (import.meta.env.DEV ? "" : configuredApiUrl || "https://jombo-essaygrade.fly.dev").replace(/\/$/, "");
 
 export default API_URL;
