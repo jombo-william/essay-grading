39ea7c3d182a7fc49e8123a28a567cd1b22312d5
TNYASULU <bed-com-26-21>
2026-05-29 02:53:55 +0200

    updated vite

--- main-folder/vite.config.js
+++ main-folder/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://jombo-essaygrade.fly.dev',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
